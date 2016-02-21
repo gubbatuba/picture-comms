9a2d3407015c7e17717824bb5f8c57c7eca70225
bramanudom <bramanud@wellesley.edu>
2016-02-21 08:44:55 -0500

    UIUpdates

---
+++ WHACKProject/app/src/main/java/com/example/pet/whackproject/LanguageSpinner.java
@@ -0,0 +1,26 @@
+package com.example.pet.whackproject;
+
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import com.example.pet.whackproject.GetLanguages;
+import com.example.pet.whackproject.R;
+
+/**
+ * Created by pratool on 2/21/16.
+// */
+//
+//public class LanguageSpinner {
+//
+//    public void setSpinnerElements (String response){
+//        Spinner dropdown = (Spinner)findViewById(R.id.langaugesSpinner);
+////        new ArrayList<String> elements = new ArrayList<String>;
+////        ArrayList.add(input);
+////        String[] items = new String[]{"1", "2", "three"};
+//        //String[] test = new String[] {input};
+//
+//        String[] test = GetLanguages.extractLangs(response);
+//        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, test);
+//        dropdown.setAdapter(adapter);
+//    }
+//}
